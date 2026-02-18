90cbc391d696301fb0f093ebbd23106c12b6ad79
EgoistD <155609454+EgoistD@users.noreply.github.com>
2026-02-18 21:28:14 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //this is a scrap file :) go to my html file, copy it... and then paste on raw.githack.com then it will generate you usable link for my file :) happy coding
-       System.out.println("Hello HackClub! Vineet this side");
-     }
-
-}
